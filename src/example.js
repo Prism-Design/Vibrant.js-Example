48d8beb03160ebdef14956e79e0d5828ef07e4a5
Leonardo Taehwan Kim <leonardo@thefinestartist.com>
2017-01-15 15:46:49 +0900

    Update

--- src/example.js
+++ src/example.js
@@ -7,8 +7,3 @@ Vibrant.from('input/yosemite.png').getPalette(function(err, palette) {
   palette_generation_time = new Date().getTime() - palette_generation_time;
   console.log('Palette created: ' + palette_generation_time + ' millisecond');
 });
-
-var image_read_time = new Date().getTime();
-var image = new CanvasImage('input/yosemite.png');
-image_read_time = new Date().getTime() - image_read_time;
-console.log('Image read: ' + image_read_time + ' millisecond');
